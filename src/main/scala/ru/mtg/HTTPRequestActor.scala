9f32fde171a9eb4934629e293f837c4629aa656b
Alexey Romanchuk <romanchuk-as@ess54.ru>
2012-07-24 17:55:24 +0700

    Typed actors with routing

--- src/main/scala/ru/mtg/HTTPRequestActor.scala
+++ src/main/scala/ru/mtg/HTTPRequestActor.scala
@@ -1,11 +1,35 @@
 package ru.mtg
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor.{TypedActor, ActorLogging, Actor}
 import org.apache.commons.io.IOUtils
 import java.net.URI
-import akka.event.Logging
+import akka.event.{LogSource, Logging}
+import akka.dispatch.{Promise, Future}
+import akka.actor.TypedActor.PreStart
 
 case class HTTPRequestResult(url : String, text : String)
+
+trait HTTPClient {
+  def fetch(url: String) : Future[HTTPRequestResult]
+}
+
+class HTTPClientImpl(val name: String) extends HTTPClient with PreStart with LogSource[HTTPClientImpl] {
+  import akka.actor.TypedActor.dispatcher
+
+  def this() = this("default")
+
+  def fetch(url: String) = Promise.successful( {
+    println("fetch")
+    new HTTPRequestResult(url, IOUtils.toString(new URI(url)))
+  })
+
+  def preStart() {
+    println("Starting HTTP client")
+  }
+
+  def genString(t: HTTPClientImpl) = this.toString
+}
+
 
 class HTTPRequestActor extends Actor with ActorLogging {
 
--- src/main/scala/ru/mtg/Sandbox.scala
+++ src/main/scala/ru/mtg/Sandbox.scala
@@ -3,21 +3,37 @@ package ru.mtg
 import akka.actor._
 import akka.pattern._
 import akka.util.{Duration, Timeout}
-import akka.routing.FromConfig
 import com.typesafe.config.ConfigFactory
-import akka.dispatch.{Await, Futures, Future}
+import akka.dispatch.{Await, Future}
+import akka.routing.RoundRobinRouter
 
 
 object Sandbox extends App {
   val config = ConfigFactory.load
   val system = ActorSystem("TestSystem", config.getConfig("test"))
   implicit val timeout = Timeout(10000000)
-  val actor: ActorRef = system.actorOf(Props[HTTPRequestActor].withRouter(FromConfig()), "http-router")
-  val futures = (1 until 10).map(x => { (actor ? ("http://sales.starcitygames.com/carddisplay.php?product=411" + x)).asInstanceOf[Future[HTTPRequestResult]]})
+  //  val actor: ActorRef = system.actorOf(Props[HTTPRequestActor].withRouter(FromConfig()), "http-router")
+  //  val futures = (1 until 10).map(x => { (actor ? ("http://sales.starcitygames.com/carddisplay.php?product=411" + x)).asInstanceOf[Future[HTTPRequestResult]]})
+  val actors = (1 to 3)
+    .map(i => {
+      println("create" + i)
+      val a : HTTPClient = TypedActor(system).typedActorOf(TypedProps[HTTPClientImpl].withTimeout(timeout), "http-router222" + i)
+      TypedActor(system).getActorRefFor(a)
+    })
+
+  val router: RoundRobinRouter = RoundRobinRouter(actors.toIterable)
+
+  val actor : HTTPClient = TypedActor(system).typedActorOf(
+    TypedProps[HTTPClientImpl].withTimeout(timeout),
+    system.actorOf(new Props().withRouter(router), "aaaa")
+  )
+//  val actor : HTTPClient = TypedActor(system).typedActorOf(TypedProps[HTTPClientImpl].withTimeout(timeout), "http-router111")
+  val futures = (1 until 10).map(x => {
+    val url = "http://sales.starcitygames.com/carddisplay.php?product=411" + x
+    actor.fetch(url)
+  })
   implicit val d = system.dispatcher
   Await.result(Future.sequence(futures), Duration.Inf).foreach(result =>
     println(result.url)
   )
-//    .foreach(future => {future.onComplete(x=>println("got future!"))})
-
 }
