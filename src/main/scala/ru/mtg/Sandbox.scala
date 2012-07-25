20531a92355773a6b4a92f64993406f28a99317a
Alexey Romanchuk <romanchuk-as@ess54.ru>
2012-07-25 11:52:08 +0700

    Typed actors with routing - cleanup

--- src/main/scala/ru/mtg/Sandbox.scala
+++ src/main/scala/ru/mtg/Sandbox.scala
@@ -1,7 +1,6 @@
 package ru.mtg
 
 import akka.actor._
-import akka.pattern._
 import akka.util.{Duration, Timeout}
 import com.typesafe.config.ConfigFactory
 import akka.dispatch.{Await, Future}
@@ -12,28 +11,16 @@ object Sandbox extends App {
   val config = ConfigFactory.load
   val system = ActorSystem("TestSystem", config.getConfig("test"))
   implicit val timeout = Timeout(10000000)
-  //  val actor: ActorRef = system.actorOf(Props[HTTPRequestActor].withRouter(FromConfig()), "http-router")
-  //  val futures = (1 until 10).map(x => { (actor ? ("http://sales.starcitygames.com/carddisplay.php?product=411" + x)).asInstanceOf[Future[HTTPRequestResult]]})
-  val actors = (1 to 3)
-    .map(i => {
-      println("create" + i)
-      val a : HTTPClient = TypedActor(system).typedActorOf(TypedProps[HTTPClientImpl].withTimeout(timeout), "http-router222" + i)
-      TypedActor(system).getActorRefFor(a)
-    })
-
-  val router: RoundRobinRouter = RoundRobinRouter(actors.toIterable)
-
-  val actor : HTTPClient = TypedActor(system).typedActorOf(
+  val actors = (1 to 3).map(i => TypedActor(system).getActorRefFor(TypedActor(system).typedActorOf(TypedProps[HTTPClientImpl].withTimeout(timeout))))
+  val actor: HTTPClient = TypedActor(system).typedActorOf(
     TypedProps[HTTPClientImpl].withTimeout(timeout),
-    system.actorOf(new Props().withRouter(router), "aaaa")
+    system.actorOf(new Props().withRouter(RoundRobinRouter(actors.toIterable)))
   )
-//  val actor : HTTPClient = TypedActor(system).typedActorOf(TypedProps[HTTPClientImpl].withTimeout(timeout), "http-router111")
   val futures = (1 until 10).map(x => {
     val url = "http://sales.starcitygames.com/carddisplay.php?product=411" + x
     actor.fetch(url)
   })
   implicit val d = system.dispatcher
-  Await.result(Future.sequence(futures), Duration.Inf).foreach(result =>
-    println(result.url)
-  )
+  Await.result(Future.sequence(futures), Duration.Inf)
+    .foreach(result => println(result.url))
 }
